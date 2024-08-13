10de4617fbceaa6cb5df8385357227f80bfcc380
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-13 13:21:33 +0430

    colors added

---
+++ script.js
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+// eslint-disable-next-line no-unused-vars
+const projectName = 'random-quote-machine';
+let quotesData;
+
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+];
