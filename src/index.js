03d142cfbd191c9e1b8a19011d298ed182ca54f4
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-17 19:01:24 +0430

    linter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 // eslint-disable-next-line import/extensions
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// eslint-disable-next-line import/extensions
 import App from './App';
+// eslint-disable-next-line import/extensions
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
