8dc6f76fd19a7e62124845367cb8451415ab1ed1
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-17 19:03:52 +0430

    linters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 // eslint-disable-next-line import/extensions
 import ReactDOM from 'react-dom/client';
@@ -6,12 +7,12 @@ import './index.css';
 import App from './App';
 // eslint-disable-next-line import/extensions
 import reportWebVitals from './reportWebVitals';
-
+/* eslint-enable no-unused-vars */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // Report web vitals metrics
